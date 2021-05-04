904f0b9ccd0b465b88896daa2f1df94dcb6b4a49
Kazuki Oishi <oishikazuki@gmail.com>
2021-05-05 02:13:29 +0900

    add test.

--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module discord-notion-expander
 go 1.16
 
 require (
-	github.com/bwmarrin/discordgo v0.23.2 // indirect
-	github.com/joho/godotenv v1.3.0 // indirect
-	github.com/kjk/notionapi v0.0.0-20210416062710-878a08dbe82d // indirect
+	github.com/alecthomas/assert v0.0.0-20170929043011-405dbfeb8e38
+	github.com/bwmarrin/discordgo v0.23.2
+	github.com/google/go-cmp v0.5.5 // indirect
+	github.com/joho/godotenv v1.3.0
+	github.com/kjk/notionapi v0.0.0-20210416062710-878a08dbe82d
+	github.com/pkg/errors v0.9.1 // indirect
+	gotest.tools v2.2.0+incompatible // indirect
 )
---
+++ utils/utils_test.go
@@ -0,0 +1,47 @@
+package utils
+
+import (
+	"github.com/alecthomas/assert"
+	"testing"
+)
+
+func TestGetNotionUrlRegex(t *testing.T) {
+	assert.True(t, getNotionUrlRegex("https://www.notion.so/test/") == "^https://www.notion.so/test/(.*-([a-zA-Z0-9]*)|([a-zA-Z0-9]*))$")
+	assert.True(t, getNotionUrlRegex("https://www.notion.so/test") == "^https://www.notion.so/test/(.*-([a-zA-Z0-9]*)|([a-zA-Z0-9]*))$")
+}
+
+var paramsTestGetNotionPageIdFromMessage = []struct {
+	baseUrl string
+	content string
+	match   bool
+	pageId  string
+}{
+	{
+		baseUrl: "https://www.notion.so/test/",
+		content: "https://www.notion.so/test/Test-page-all-c969c9455d7c4dd79c7f860f3ace6429",
+		match:   true,
+		pageId:  "c969c9455d7c4dd79c7f860f3ace6429",
+	},
+	{
+		baseUrl: "https://www.notion.so/test/",
+		content: "https://www.notion.so/test/c969c9455d7c4dd79c7f860f3ace6429",
+		match:   true,
+		pageId:  "c969c9455d7c4dd79c7f860f3ace6429",
+	},
+	{
+		baseUrl: "https://www.notion.so/test/",
+		content: "うえしゃまぁああああああ",
+		match:   false,
+		pageId:  "",
+	},
+}
+
+func TestGetNotionPageIdFromMessage(t *testing.T) {
+	for _, testItem := range paramsTestGetNotionPageIdFromMessage {
+		match, pageId := GetNotionPageIdFromMessage(testItem.baseUrl, testItem.content)
+		assert.Equal(t, testItem.match, match)
+		if testItem.match {
+			assert.Equal(t, testItem.pageId, pageId)
+		}
+	}
+}
